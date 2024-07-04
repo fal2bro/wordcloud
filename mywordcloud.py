87400268fff0c199f117987a536025e6e6bc7acd
fal2bro <arai261@stu.kanazawa-u.ac.jp>
2024-07-05 07:24:33 +0900

    助詞などを除外

--- mywordcloud.py
+++ mywordcloud.py
@@ -4,11 +4,12 @@ import MeCab
 
 def CreateWordcloud(text):
     # MeCabを使用して日本語の単語を抽出
-    mecab = MeCab.Tagger("-Ochasen")
-    text_wakati = mecab.parse(text)
-    fpath="./ipaexg00401\ipaexg.ttf"
+    #mecab = MeCab.Tagger("-Owakati")
+    #text_wakati = mecab.parse(text)
+    filtered_text = filter_words(text)
+    font_path=".\wordcloud\ipaexg00401\ipaexg.ttf"
     # WordCloudオブジェクトを作成
-    wordcloud = WordCloud(font_path=fpath,width=800, height=400, background_color='white', colormap='viridis', collocations=False).generate(text_wakati)
+    wordcloud = WordCloud(width=800, height=400, background_color='white', colormap='viridis', collocations=False, font_path=font_path).generate(filtered_text)
 
     # 画像を描画するためのプロットを作成
     plt.figure(figsize=(10, 5))
@@ -17,6 +18,28 @@ def CreateWordcloud(text):
 
     # 画像を表示
     plt.show()
+
+
+
+def filter_words(text):
+    mecab = MeCab.Tagger()
+    node = mecab.parseToNode(text)
+    words = []
+    
+    # ストップワードリスト
+    stop_words = {'の', 'が', 'は', 'に', 'を', 'と', 'も', 'で', 'から', 'や', 'ば', 'へ', 'けど', 'です', 'ます', 'この', 'その', 'あの', 'だ', 'いる', 'ある', 'する', 'こと'}
+    
+    while node:
+        word = node.surface
+        pos = node.feature.split(',')[0]
+        
+        # 助詞、助動詞、記号、接続詞、ストップワードを除外
+        if pos not in ['助詞', '助動詞', '記号', '接続詞'] and word not in stop_words:
+            words.append(word)
+        node = node.next
+    
+    return ' '.join(words)
+
 
 if __name__ == '__main__':
     filename = input("ファイルパスを指定してください: ")
